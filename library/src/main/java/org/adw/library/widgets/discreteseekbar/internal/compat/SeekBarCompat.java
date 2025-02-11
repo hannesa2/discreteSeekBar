0a8d4ab4e69944b5d7a5aa27b213beffe26d4cf5
Hannes Achleitner <hannes.software@gmx.at>
2025-02-11 14:23:24 +0100

    Modernize

--- library/src/main/java/org/adw/library/widgets/discreteseekbar/internal/compat/SeekBarCompat.java
+++ library/src/main/java/org/adw/library/widgets/discreteseekbar/internal/compat/SeekBarCompat.java
@@ -20,11 +20,12 @@ import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.RippleDrawable;
 import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.View;
 import android.view.ViewParent;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import org.adw.library.widgets.discreteseekbar.internal.drawable.AlmostRippleDrawable;
 import org.adw.library.widgets.discreteseekbar.internal.drawable.MarkerDrawable;
